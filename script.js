c963c65dc442530d42bb033679012d6dec71724a
Helena Dusza <helena.dusza@gmail.com>
2021-08-11 11:09:04 +0100

    menus with ni=umbers

--- script.js
+++ script.js
@@ -3,16 +3,57 @@ const burgerOpen = document.querySelector('.burger-icon'),
   burger = document.querySelector('.burger'),
   navList = document.querySelector('.nav-list'),
   menuBtn = document.querySelectorAll('.btn-menu'),
-  menuIndex = document.querySelector('.menu-index');
+  menuIndex = document.querySelector('.menu-index'),
+  logo = document.querySelector('.logo'),
+  navSticky = document.querySelector('.nav-list-dektop-wrapper'),
+  header = document.querySelector('header'),
+  navBackground = document.querySelector('.nav-list-dektop-wrapper::before'),
+  ourMenus = document.querySelector('.text3'),
+  menusLink = document.querySelectorAll('.menus');
+const sectionOneOptions = {
+  rootMargin: '-600px 0px 0px 0px',
+};
+const sectionTwoOptions = {
+  rootMargin: '-100px 0px 0px 0px',
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      navSticky.classList.add('sticky');
+
+      if (window.matchMedia('(min-width: 1200px)').matches) {
+        logo.style.transform = 'scale(0.6)';
+      }
+    } else {
+      navSticky.classList.remove('sticky');
+      logo.style.transform = 'scale(1)';
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(header);
 
 burger.addEventListener('click', toggleMobileMenu);
 menuBtn.forEach(item => item.addEventListener('click', toggleMenu));
+menusLink.forEach(item => item.addEventListener('click', goToMenus));
 // menuBtn.addEventListener('click', toggleMenu);
 function toggleMobileMenu() {
   burgerClose.classList.toggle('show');
   burgerOpen.classList.toggle('show');
   navList.classList.toggle('show');
 }
+function goToMenus() {
+  ourMenus.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
 function toggleMenu(e) {
-  e.target.parentElement.nextElementSibling.classList.toggle('show');
+  if (window.matchMedia('(max-width: 800px)').matches) {
+    e.target.parentElement.nextElementSibling.classList.toggle('show');
+  } else {
+    ourMenus.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
 }
